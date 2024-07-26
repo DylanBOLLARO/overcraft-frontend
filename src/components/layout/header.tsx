49b2eadce72eb2735472469c0a4e79ea02d3689f
Dylan <bollarodylan@gmail.com>
2024-07-26 22:48:39 +0200

    Adding title of website and fake search bar

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,13 +1,27 @@
 import { UserNav } from "./user-nav";
 import ThemeToggle from "./ThemeToggle/theme-toggle";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
 
 export default function Header() {
 	return (
 		<div className="supports-backdrop-blur:bg-background/60 fixed left-0 right-0 top-0 z-20 border-b bg-background/95 backdrop-blur">
-			<nav className="flex h-14 px-4 items-center gap-2">
-				<div className="flex items-center gap-2 ml-auto">
-					<UserNav />
-					<ThemeToggle />
+			<nav className="flex h-14 px-4 items-center gap-2 relative">
+				<h4 className="text-xl font-semibold tracking-wider opacity-75 ml-5">
+					Overcraft
+				</h4>
+
+				<div className="flex items-center ml-auto gap-10">
+					<div className="flex w-full max-w-sm items-center space-x-2">
+						<Input type="email" placeholder="Email" />
+						<Button type="submit" variant={"outline"}>
+							Search
+						</Button>
+					</div>
+					<div className="ml-10 flex items-center space-x-2">
+						<UserNav />
+						<ThemeToggle />
+					</div>
 				</div>
 			</nav>
 		</div>
