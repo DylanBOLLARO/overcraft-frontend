064ae3cc7f6fb0350bfd1d4a5ca3084aa5e29ed6
Bollaro Dylan <bollarodylan@gmail.com>
2025-03-11 18:02:42 +0100

    fix redirect

--- next.config.mjs
+++ next.config.mjs
@@ -13,25 +13,54 @@ const nextConfig = {
             },
         ],
     },
+
+    async redirects() {
+        return process.env.NODE_ENV === 'production'
+            ? [
+                  {
+                      source: '/auth/login',
+                      destination: `${process.env.NEXT_PUBLIC_BACKEND_OVERCRAFT_URL}/auth/login`,
+                      permanent: false,
+                  },
+                  {
+                      source: '/callback',
+                      destination: `${process.env.NEXT_PUBLIC_BACKEND_OVERCRAFT_URL}/callback`,
+                      permanent: false,
+                  },
+                  {
+                      source: '/auth/user',
+                      destination: `${process.env.NEXT_PUBLIC_BACKEND_OVERCRAFT_URL}/auth/user`,
+                      permanent: false,
+                  },
+                  {
+                      source: '/auth/logout',
+                      destination: `${process.env.NEXT_PUBLIC_BACKEND_OVERCRAFT_URL}/auth/logout`,
+                      permanent: false,
+                  },
+              ]
+            : []
+    },
     async rewrites() {
-        return [
-            {
-                source: '/auth/logout',
-                destination: `${process.env.NEXT_PUBLIC_BACKEND_OVERCRAFT_URL}/auth/logout`,
-            },
-            {
-                source: '/auth/login',
-                destination: `${process.env.NEXT_PUBLIC_BACKEND_OVERCRAFT_URL}/auth/login`,
-            },
-            {
-                source: '/auth/user',
-                destination: `${process.env.NEXT_PUBLIC_BACKEND_OVERCRAFT_URL}/auth/user`,
-            },
-            {
-                source: '/callback',
-                destination: `${process.env.NEXT_PUBLIC_BACKEND_OVERCRAFT_URL}/callback`,
-            },
-        ]
+        return process.env.NODE_ENV === 'production'
+            ? []
+            : [
+                  {
+                      source: '/auth/login',
+                      destination: `${process.env.NEXT_PUBLIC_BACKEND_OVERCRAFT_URL}/auth/login`,
+                  },
+                  {
+                      source: '/callback',
+                      destination: `${process.env.NEXT_PUBLIC_BACKEND_OVERCRAFT_URL}/callback`,
+                  },
+                  {
+                      source: '/auth/user',
+                      destination: `${process.env.NEXT_PUBLIC_BACKEND_OVERCRAFT_URL}/auth/user`,
+                  },
+                  {
+                      source: '/auth/logout',
+                      destination: `${process.env.NEXT_PUBLIC_BACKEND_OVERCRAFT_URL}/auth/logout`,
+                  },
+              ]
     },
 }
 
