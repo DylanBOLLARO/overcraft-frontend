eda73e7875500cc566c9b77d12e9fd9a75dc0960
Bollaro Dylan <bollarodylan@gmail.com>
2025-03-22 10:48:39 +0100

    add motion on build card appear

--- src/components/builds-list.tsx
+++ src/components/builds-list.tsx
@@ -1,11 +1,12 @@
 import { BuildItem } from './build-item'
+import { AnimatedGroup } from './ui/animated-group'
 
 export function BuildsList({ builds, update = false }: any) {
     return (
-        <div className="grid 2xl:grid-cols-3 lg:grid-cols-2 flex-wrap gap-2">
+        <AnimatedGroup className="grid 2xl:grid-cols-3 lg:grid-cols-2 flex-wrap gap-2">
             {builds?.map((build: any) => (
                 <BuildItem key={build?.slug} build={build} update={update} />
             ))}
-        </div>
+        </AnimatedGroup>
     )
 }
